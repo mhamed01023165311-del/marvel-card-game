c2fcdbdfa77951c1b003ade0c5ff4b891909b4fa
Falcon.1 <mhamed01023165311@gmail.com>
2026-01-05 14:41:25 +0200

    Update CombatSystem.ts

--- src/game/CombatSystem.ts
+++ src/game/CombatSystem.ts
@@ -1,1 +1,12 @@
+import { Character } from "./Character";
 
+export function fight(attacker: Character, defender: Character) {
+  const damage =
+    attacker.card.attack - defender.card.defense;
+
+  if (damage > 0) {
+    defender.health -= damage;
+    return damage;
+  }
+  return 0;
+}

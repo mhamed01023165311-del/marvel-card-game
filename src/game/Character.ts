b085fd20055b38667faa8cc222108040c5e80f78
Falcon.1 <mhamed01023165311@gmail.com>
2026-01-05 14:38:57 +0200

    Update Character.ts

--- src/game/Character.ts
+++ src/game/Character.ts
@@ -1,1 +1,18 @@
+import { Card } from "./Card";
+import { Tile } from "./Tile";
 
+export class Character {
+  card: Card;
+  health: number;
+
+  constructor(card: Card) {
+    this.card = card;
+    this.health = 100;
+  }
+
+  applyTile(tile: Tile) {
+    if (tile.bonus === "attack") this.card.attack += 10;
+    if (tile.bonus === "defense") this.card.defense += 10;
+    if (tile.bonus === "range") this.card.range += 1;
+  }
+}

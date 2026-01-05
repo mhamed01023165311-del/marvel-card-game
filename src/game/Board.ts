55f83cbd22c5a5c82cc6368b051fe70a2b7f9830
Falcon.1 <mhamed01023165311@gmail.com>
2026-01-05 14:22:49 +0200

    Update Board.ts

--- src/game/Board.ts
+++ src/game/Board.ts
@@ -1,1 +1,24 @@
+import { Tile } from "./Tile";
 
+export class Board {
+  width = 4;
+  height = 4;
+  tiles: Tile[] = [];
+
+  constructor() {
+    this.generate();
+  }
+
+  generate() {
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        this.tiles.push({
+          x,
+          y,
+          owner: y < this.height / 2 ? "player" : "enemy",
+          occupied: false,
+        });
+      }
+    }
+  }
+    }

30edab42f0bb6b011c4f0395c17b898494b74eda
Falcon.1 <mhamed01023165311@gmail.com>
2026-01-05 14:41:43 +0200

    Update GameState.ts

--- src/game/GameState.ts
+++ src/game/GameState.ts
@@ -1,1 +1,15 @@
+import { Board } from "./Board";
+import { Player } from "./Player";
+import { TurnManager } from "./TurnManager";
 
+export class GameState {
+  board = new Board();
+  turn = new TurnManager();
+  player: Player;
+  enemy: Player;
+
+  constructor(player: Player, enemy: Player) {
+    this.player = player;
+    this.enemy = enemy;
+  }
+}

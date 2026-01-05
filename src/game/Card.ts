2e205a8ef01061572c92e4135864925e5dde343e
Falcon.1 <mhamed01023165311@gmail.com>
2026-01-05 14:38:37 +0200

    Update Card.ts

--- src/game/Card.ts
+++ src/game/Card.ts
@@ -1,1 +1,15 @@
+export type CardRole =
+  | "attack"
+  | "defense"
+  | "ranged"
+  | "attack-defense"
+  | "attack-ranged";
 
+export interface Card {
+  id: string;
+  name: string;
+  role: CardRole;
+  attack: number;
+  defense: number;
+  range: number;
+}

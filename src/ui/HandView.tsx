bb87c36e6e9f0b9bab478bd6762613e916643428
Falcon.1 <mhamed01023165311@gmail.com>
2026-01-05 14:45:34 +0200

    Update HandView.tsx

--- src/ui/HandView.tsx
+++ src/ui/HandView.tsx
@@ -1,1 +1,24 @@
+import React from "react";
+import { Card } from "../game/Card";
+import { CardView } from "./CardView";
 
+interface HandViewProps {
+  hand: Card[];
+  selectedCard: Card | null;
+  onSelectCard: (card: Card) => void;
+}
+
+export const HandView: React.FC<HandViewProps> = ({ hand, selectedCard, onSelectCard }) => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", marginTop: 10 }}>
+      {hand.map(card => (
+        <CardView
+          key={card.id}
+          card={card}
+          isSelected={selectedCard?.id === card.id}
+          onSelect={onSelectCard}
+        />
+      ))}
+    </div>
+  );
+};
